8dcaaeb7699dcc68fb7686177c450e635233d127
s-fedorov <s.fedorov@dostaevsky.ru>
2019-12-12 18:08:59 +0300

    Add User controller unit tests

---
+++ src/MicroserviceFdrv.Tests/UserControllerUnitTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using MicroserviceFdrv.Controllers;
+
+namespace MicroserviceFdrv.Tests
+{
+    public class UserControllerUnitTest
+    {
+        [Fact]
+        public void Register()
+        {
+            // Arrange
+            UserController controller = new UserController();
+            string username = "user";
+
+            // Act
+            IActionResult result = controller.Register(username);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void RegisterWithPassword()
+        {
+            // Arrange
+            UserController controller = new UserController();
+            string username = "user0", password = "asd";
+
+            // Act
+            IActionResult result = controller.Register(username, password);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void Login()
+        {
+            // Arrange
+            UserController controller = new UserController();
+            string username = "user";
+
+            // Act
+            IActionResult result = controller.Login(username);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void LoginPassword()
+        {
+            // Arrange
+            UserController controller = new UserController();
+            string username = "user0", password = "asd";
+
+            // Act
+            IActionResult result = controller.Login(username, password);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void LoginWrong()
+        {
+            // Arrange
+            UserController controller = new UserController();
+            string username = "user0", password = "a";
+
+            // Act
+            IActionResult result = controller.Login(username, password);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void Logout()
+        {
+            // Arrange
+            UserController controller = new UserController();
+            int id = 1;
+
+            // Act
+            IActionResult result = controller.Logout(id);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
